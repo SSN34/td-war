6a0b6c7103c6a617791fdd2071b18ef2563c1a31
SN <ishu.soumya@gmail.com>
2022-10-09 01:06:43 +0530

    added tile sets

---
+++ tileSet/Houses.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="Houses" tilewidth="32" tileheight="32" tilecount="10" columns="5">
+ <image source="C:/Users/Soumya S Narayan/Downloads/Houses.png" width="176" height="64"/>
+</tileset>
